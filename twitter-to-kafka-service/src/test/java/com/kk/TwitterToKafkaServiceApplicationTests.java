a5bb5ab4d3ac7760f07301bc4f43cf102e3b4773
Kapil <kanurkapil@gmail.com>
2025-03-03 20:34:58 +0530

    Added twitter to kafka service project

---
+++ twitter-to-kafka-service/src/test/java/com/kk/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TwitterToKafkaServiceApplicationTests {
+
+    @Test
+    public void contextLoad() {
+        //The test passes if the application context loads successfully
+    }
+}
